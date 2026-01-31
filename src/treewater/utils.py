922f2778c1328b01bcbcbe41064afb91274833d9
Sally Tao <jinyant10@gmail.com>
2026-01-31 20:24:26 +0000

    train all

--- src/treewater/utils.py
+++ src/treewater/utils.py
@@ -2375,7 +2375,11 @@ def train_transformer_rolling_loss(model, train_loader, val_loader, train_df, va
         avg_vloss_f = avg_vloss.item() if hasattr(avg_vloss, "item") else float(avg_vloss)
 
         val_rmse = math.sqrt(val_sse / val_n) if val_n > 0 else float("nan")
-        print(f'Train loss:{avg_loss:.4f}, Train RMSE:{train_rmse:.4f}, Val loss:{avg_vloss:.4f}, Val RMSE:{val_rmse:.4f}, p_tf:{p_tf:.4f}')
+        if scheduled:
+            print(f'Train loss:{avg_loss:.4f}, Train RMSE:{train_rmse:.4f}, Val loss:{avg_vloss:.4f}, Val RMSE:{val_rmse:.4f}, p_tf:{p_tf:.4f}')
+            hisotory["p_tf"].append(float(p_tf))
+        else:
+            print(f'Train loss:{avg_loss:.4f}, Train RMSE:{train_rmse:.4f}, Val loss:{avg_vloss:.4f}, Val RMSE:{val_rmse:.4f}')
         # Log the running loss averaged per batch
         # for both training and validation
 
